c3a5f6d581258e487f20bcef7842dbe68a508141
Priya R <priyareddyece97@gmail.com>
2021-11-24 19:24:26 +0530

    payment gateway added

--- src/Component/ProductDetails.js
+++ src/Component/ProductDetails.js
@@ -1,5 +1,5 @@
 import { ProductContext } from "../Context/ProductContext";
-import React, { useContext,useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { Container, Button, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -7,20 +7,20 @@ import axios from "axios";
 export default function ProductDetails() {
   const API_URL = "https://shopping-app-api01.herokuapp.com/postproduct";
 
-   // context for product details
+  // context for product details
   const context = useContext(ProductContext);
 
-  const addBuynowProducts = async() =>{
-    try{
-      const {buyNowItem:{id,product,picture,rate}} = context;
+  const addBuynowProducts = async () => {
+    try {
+      const { buyNowItem: { id, product, picture, rate } } = context;
       const mailid = localStorage.getItem("user");
-      
-      const {data} = await axios.post(API_URL,{
-        mailid,id,product,picture,rate
+
+      const { data } = await axios.post(API_URL, {
+        mailid, id, product, picture, rate
       })
-      
+
       console.log(data);
-    }catch(err){
+    } catch (err) {
       console.log(err);
     }
   }
@@ -30,16 +30,18 @@ export default function ProductDetails() {
       <Container>
         <Table responsive="lg">
           <tr>
-            <td> <div><img variant="top" src={context.buyNowItem.picture} alt ="pic" height={330} width={330} />
-            <Link to="/modal"><Button type="button" size="sm" className="buynowBtn" onClick={addBuynowProducts}>Buy Now</Button></Link>
+            <td> <div><img variant="top" src={context.buyNowItem.picture} alt="pic" height={330} width={330} />
+              <Link to={{
+                pathname: 'https://rzp.io/l/uVXB2JhLfx',
+              }} target="_blank"><Button type="button" size="sm" className="buynowBtn" onClick={addBuynowProducts}>Buy Now</Button></Link>
             </div></td>
             <td> <h1 className="mt-2">{context.buyNowItem.product}</h1>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
                 vestibulum ante eu enim tincidunt, quis ultricies orci elementum.
-                Nulla facilisi. Ut et ante quis lacus dictum sodales ut in libero.
+                Nula facilisi. Ut et ante quis lacus dictum sodales ut in libero.
                 Ut et ante quis lacus dictum sodales ut in libero.Nulla facilisi.
-                Ut et ante quis.
+                Ut et ante quis.l
               </p>
               <div className="d-flex">
                 <p>Price : {context.buyNowItem.rate} </p>
--- src/Pages/Card.js
+++ src/Pages/Card.js
@@ -6,27 +6,27 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function Cardfn() {
-    const API_URL = "http://localhost:3001/postproduct";
+    const API_URL = "https://shopping-app-api01.herokuapp.com/postproduct";
     const context = useContext(ProductContext);
     const history = useHistory();
-        useEffect(()=>{
-         if(!localStorage.getItem("user")){
+    useEffect(() => {
+        if (!localStorage.getItem("user")) {
             history.push("/")
-         }   
-        },[])
+        }
+    }, [])
     //Remove card item function
-    const onDeleteByIndex = async(item) => {
-     await context.HandleRemoveCardFn(item);
-        try{
-              const {id,product,picture,rate} = item;
-              const mailid = localStorage.getItem("user");
-              const {data} = await axios.post(API_URL,{
-                mailid,id,product,picture,rate
-              })
-              console.log(data);
-            }catch(err){
-              console.log(err);
-            }
+    const onDeleteByIndex = async (item) => {
+        await context.HandleRemoveCardFn(item);
+        try {
+            const { id, product, picture, rate } = item;
+            const mailid = localStorage.getItem("user");
+            const { data } = await axios.post(API_URL, {
+                mailid, id, product, picture, rate
+            })
+            console.log(data);
+        } catch (err) {
+            console.log(err);
+        }
     }
 
 
@@ -36,28 +36,30 @@ export default function Cardfn() {
                 <Row md={4} className="g-4">
 
                     {
-                     context.carditem.length == 0 ? <h1 className="EmptyCard"><i className="fas fa-shopping-cart"></i> Card is Empty</h1> : (
-                    context.carditem.map((item, index) => {
-                        return (
-                            <Col key={index}>
-                                <Card>
-                                    <Card.Img variant="top" src={item.picture} height={290} width={450} />
-                                    <Card.Body>
-                                        <Card.Title>{item.product}</Card.Title>
-                                        <Card.Text>
-                                            {item.rate}
-                                        </Card.Text>
-                                    </Card.Body>
-                                    <Card.Footer>
-                                        <Link to="/modal"><Button type="button" size="sm" onClick={() => { onDeleteByIndex(item) }}>Buy Now</Button></Link>
-                                        <Button variant="secondary" size="sm" onClick={() => { onDeleteByIndex(item) }}>Remove To Card</Button>
-                                    </Card.Footer>
-                                </Card>
-                            </Col>
-                        )
-                    }
-                    )
-                     )}
+                        context.carditem.length == 0 ? <h1 className="EmptyCard"><i className="fas fa-shopping-cart"></i> Card is Empty</h1> : (
+                            context.carditem.map((item, index) => {
+                                return (
+                                    <Col key={index}>
+                                        <Card>
+                                            <Card.Img variant="top" src={item.picture} height={290} width={450} />
+                                            <Card.Body>
+                                                <Card.Title>{item.product}</Card.Title>
+                                                <Card.Text>
+                                                    {item.rate}
+                                                </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                                <Link to={{
+                                                    pathname: 'https://rzp.io/l/uVXB2JhLfx',
+                                                }} target="_blank"><Button type="button" size="sm" onClick={() => { onDeleteByIndex(item) }}>Buy Now</Button></Link>
+                                                <Button variant="secondary" size="sm" onClick={() => { onDeleteByIndex(item) }}>Remove To Card</Button>
+                                            </Card.Footer>
+                                        </Card>
+                                    </Col>
+                                )
+                            }
+                            )
+                        )}
                 </Row>
             </Container>
         </>
